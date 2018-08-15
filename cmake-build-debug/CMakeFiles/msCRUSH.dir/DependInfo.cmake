d02fb66e3d0df8e9807985d61124a4fb7501d4eb
Yasset Perez-Riverol <ypriverol@gmail.com>
2018-08-15 07:55:46 +0100

    minor changes

---
+++ cmake-build-debug/CMakeFiles/msCRUSH.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yperez/work/github/msCRUSH/src/app/greedyCluster_omp_general_charge.cc" "/Users/yperez/work/github/msCRUSH/cmake-build-debug/CMakeFiles/msCRUSH.dir/src/app/greedyCluster_omp_general_charge.cc.o"
+  "/Users/yperez/work/github/msCRUSH/src/class/core.cc" "/Users/yperez/work/github/msCRUSH/cmake-build-debug/CMakeFiles/msCRUSH.dir/src/class/core.cc.o"
+  "/Users/yperez/work/github/msCRUSH/src/class/distance.cc" "/Users/yperez/work/github/msCRUSH/cmake-build-debug/CMakeFiles/msCRUSH.dir/src/class/distance.cc.o"
+  "/Users/yperez/work/github/msCRUSH/src/utility/io.cc" "/Users/yperez/work/github/msCRUSH/cmake-build-debug/CMakeFiles/msCRUSH.dir/src/utility/io.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/app"
+  "../src/class"
+  "../src/test"
+  "../src/utility"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/msCRUSH.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/msCRUSH.dir/src/app/greedyCluster_omp_general_charge.cc.o"
+  "CMakeFiles/msCRUSH.dir/src/class/core.cc.o"
+  "CMakeFiles/msCRUSH.dir/src/class/distance.cc.o"
+  "CMakeFiles/msCRUSH.dir/src/utility/io.cc.o"
+  "msCRUSH.pdb"
+  "msCRUSH"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/msCRUSH.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
